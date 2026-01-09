2e333f2dcb983092663617fde6b870a8bd40ce59
Bence Szucs <bence.szucs200625@gmail.com>
2026-01-08 23:42:39 -0500

    Adds base handler function declarations

--- Pico-Camera.c
+++ Pico-Camera.c
@@ -1,13 +1,27 @@
-#include <stdio.h>
 #include "pico/stdlib.h"
+#include "pico/sync.h"
+#include "pico/multicore.h"
+#include <malloc.h>
+#define SYS_CLK_MHZ 200
 
+void cameraHandler();
+void siteHandler();
 
 int main()
 {
     stdio_init_all();
+    
+    multicore_launch_core1(cameraHandler);
 
-    while (true) {
-        printf("Hello, world!\n");
-        sleep_ms(1000);
-    }
+    
+}
+
+void cameraHandler()
+{
+
+}
+
+void siteHandler() 
+{
+
 }
